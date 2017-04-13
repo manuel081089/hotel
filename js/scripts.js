fc2263dc9f4c8ad96a98608ec16ee9766f15c9d2
pedroc <mpedroc90@gmail.com>
2017-04-13 00:47:49 -0600

    full-description

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,6 @@
 jQuery(document).ready(function($) {
-	$nav = $('.wrap>nav');
-	$menuToggle = $('.nav-toggle');
 
-	// Attaches event handler when .menu-toggle is clicked
-	$menuToggle.on('click', function(event) {
-		event.preventDefault();
-		$nav.toggleClass('navbar-open');
-	});
+	mainMenu.init();
 
-	$closeNav = $('.close-nav');
-
-	$closeNav.on('click', function(event) {
-		event.preventDefault();
-		$nav.toggleClass('navbar-open');
-	});
-
-
-
-	// Dropdown toggles
-	$dropdowns = $('.dropdown');
-	$dropdownToggle = $('.dropdown-toggle');
-
-	// Attaches event handler when .menu-toggle is clicked
-	$dropdownToggle.on('click', function(event) {
-		event.preventDefault();
-
-		$(this).parent().toggleClass('active');
-		$(this).next().next().toggleClass('show-menu');
-		
-	});
 
 });
