b0bdedf535cbb66cc41bd35d343d98bb01e723dd
Helian Rivera Hernandez <hriverahdez2011@gmail.com>
2017-04-17 13:04:16 -0400

    Fixing absolute path issue

--- js/appAngular.js
+++ js/appAngular.js
@@ -12,39 +12,39 @@
                 $stateProvider
                     .state('home', {
                         url: '/home',
-                        templateUrl: '../pages/home.html'
+                        templateUrl: 'pages/home.html'
                     })
                     .state('culturalMadrid', {
                         url: '/culturalMadrid',
-                        templateUrl: '../pages/card1.html',
+                        templateUrl: 'pages/card1.html',
                     })
                     .state('calmComfort', {
                         url: '/calmComfort',
-                        templateUrl: '../pages/card2.html',
+                        templateUrl: 'pages/card2.html',
                     })
                     .state('orfilaTaste', {
                         url: '/orfilaTaste',
-                        templateUrl: '../pages/card3.html',
+                        templateUrl: 'pages/card3.html',
                     })
                     .state('packages', {
                         url: '/packages',
-                        templateUrl: '../pages/packages.html',
+                        templateUrl: 'pages/packages.html',
                     })
                     .state('junior', {
                         url: '/juniorSuite',
-                        templateUrl: '../pages/juniorSuite.html',
+                        templateUrl: 'pages/juniorSuite.html',
                     })
                     .state('double', {
                         url: '/doubleSuite',
-                        templateUrl: '../pages/doubleSuite.html',
+                        templateUrl: 'pages/doubleSuite.html',
                     })
                     .state('deluxe', {
                         url: '/deluxeSuite',
-                        templateUrl: '../pages/deluxeSuite.html',
+                        templateUrl: 'pages/deluxeSuite.html',
                     })
                     .state('executiveMenu', {
                         url: '/executiveMenu',
-                        templateUrl: '../pages/executiveMenu.html',
+                        templateUrl: 'pages/executiveMenu.html',
                     })
 
                 $urlRouterProvider.otherwise('home');
